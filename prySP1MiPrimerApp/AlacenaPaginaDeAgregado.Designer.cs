93b1d8116b0cb49dc5bf10711e5c067965e43297
root <mgiraudo@ies21.edu.ar>
2023-04-21 14:08:42 -0300

    21/04/2023

--- prySP1MiPrimerApp/AlacenaPaginaDeAgregado.Designer.cs
+++ prySP1MiPrimerApp/AlacenaPaginaDeAgregado.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.mrcAgregadoDeProductos = new System.Windows.Forms.GroupBox();
-            this.lblNombreDeProducto = new System.Windows.Forms.Label();
-            this.textNombreDeProducto = new System.Windows.Forms.TextBox();
-            this.lstCatProductos = new System.Windows.Forms.ComboBox();
-            this.lblCategoria = new System.Windows.Forms.Label();
-            this.lblCantidad = new System.Windows.Forms.Label();
-            this.lstCantidad = new System.Windows.Forms.ComboBox();
-            this.cmdCancelar = new System.Windows.Forms.Button();
             this.cmdConfirmar = new System.Windows.Forms.Button();
+            this.cmdCancelar = new System.Windows.Forms.Button();
+            this.lstCantidad = new System.Windows.Forms.ComboBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lstCatProductos = new System.Windows.Forms.ComboBox();
+            this.textNombreDeProducto = new System.Windows.Forms.TextBox();
+            this.lblNombreDeProducto = new System.Windows.Forms.Label();
             this.mrcAgregadoDeProductos.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,56 +57,25 @@
             this.mrcAgregadoDeProductos.TabStop = false;
             this.mrcAgregadoDeProductos.Text = "Agregado de productos";
             // 
-            // lblNombreDeProducto
+            // cmdConfirmar
             // 
-            this.lblNombreDeProducto.AutoSize = true;
-            this.lblNombreDeProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombreDeProducto.Location = new System.Drawing.Point(6, 37);
-            this.lblNombreDeProducto.Name = "lblNombreDeProducto";
-            this.lblNombreDeProducto.Size = new System.Drawing.Size(146, 17);
-            this.lblNombreDeProducto.TabIndex = 0;
-            this.lblNombreDeProducto.Text = "Nombre del Producto:";
-            this.lblNombreDeProducto.Click += new System.EventHandler(this.label1_Click);
+            this.cmdConfirmar.Location = new System.Drawing.Point(175, 184);
+            this.cmdConfirmar.Name = "cmdConfirmar";
+            this.cmdConfirmar.Size = new System.Drawing.Size(75, 23);
+            this.cmdConfirmar.TabIndex = 7;
+            this.cmdConfirmar.Text = "Confirmar";
+            this.cmdConfirmar.UseVisualStyleBackColor = true;
+            this.cmdConfirmar.Click += new System.EventHandler(this.cmdConfirmar_Click);
             // 
-            // textNombreDeProducto
+            // cmdCancelar
             // 
-            this.textNombreDeProducto.Location = new System.Drawing.Point(151, 37);
-            this.textNombreDeProducto.Name = "textNombreDeProducto";
-            this.textNombreDeProducto.Size = new System.Drawing.Size(100, 20);
-            this.textNombreDeProducto.TabIndex = 1;
-            // 
-            // lstCatProductos
-            // 
-            this.lstCatProductos.FormattingEnabled = true;
-            this.lstCatProductos.Items.AddRange(new object[] {
-            "Pereceredos",
-            "No pereceredos",
-            "Bebidas",
-            "Gula"});
-            this.lstCatProductos.Location = new System.Drawing.Point(85, 72);
-            this.lstCatProductos.Name = "lstCatProductos";
-            this.lstCatProductos.Size = new System.Drawing.Size(103, 21);
-            this.lstCatProductos.TabIndex = 2;
-            // 
-            // lblCategoria
-            // 
-            this.lblCategoria.AutoSize = true;
-            this.lblCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCategoria.Location = new System.Drawing.Point(6, 73);
-            this.lblCategoria.Name = "lblCategoria";
-            this.lblCategoria.Size = new System.Drawing.Size(73, 17);
-            this.lblCategoria.TabIndex = 3;
-            this.lblCategoria.Text = "Categoria:";
-            // 
-            // lblCantidad
-            // 
-            this.lblCantidad.AutoSize = true;
-            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCantidad.Location = new System.Drawing.Point(6, 109);
-            this.lblCantidad.Name = "lblCantidad";
-            this.lblCantidad.Size = new System.Drawing.Size(68, 17);
-            this.lblCantidad.TabIndex = 4;
-            this.lblCantidad.Text = "Cantidad:";
+            this.cmdCancelar.Location = new System.Drawing.Point(25, 184);
+            this.cmdCancelar.Name = "cmdCancelar";
+            this.cmdCancelar.Size = new System.Drawing.Size(75, 23);
+            this.cmdCancelar.TabIndex = 6;
+            this.cmdCancelar.Text = "Cancelar";
+            this.cmdCancelar.UseVisualStyleBackColor = true;
+            this.cmdCancelar.Click += new System.EventHandler(this.cmdCancelar_Click);
             // 
             // lstCantidad
             // 
@@ -137,25 +106,56 @@
             this.lstCantidad.Size = new System.Drawing.Size(103, 21);
             this.lstCantidad.TabIndex = 5;
             // 
-            // cmdCancelar
+            // lblCantidad
             // 
-            this.cmdCancelar.Location = new System.Drawing.Point(25, 184);
-            this.cmdCancelar.Name = "cmdCancelar";
-            this.cmdCancelar.Size = new System.Drawing.Size(75, 23);
-            this.cmdCancelar.TabIndex = 6;
-            this.cmdCancelar.Text = "Cancelar";
-            this.cmdCancelar.UseVisualStyleBackColor = true;
-            this.cmdCancelar.Click += new System.EventHandler(this.cmdCancelar_Click);
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(6, 109);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(68, 17);
+            this.lblCantidad.TabIndex = 4;
+            this.lblCantidad.Text = "Cantidad:";
             // 
-            // cmdConfirmar
+            // lblCategoria
             // 
-            this.cmdConfirmar.Location = new System.Drawing.Point(176, 184);
-            this.cmdConfirmar.Name = "cmdConfirmar";
-            this.cmdConfirmar.Size = new System.Drawing.Size(75, 23);
-            this.cmdConfirmar.TabIndex = 7;
-            this.cmdConfirmar.Text = "Confirmar";
-            this.cmdConfirmar.UseVisualStyleBackColor = true;
-            this.cmdConfirmar.Click += new System.EventHandler(this.cmdConfirmar_Click);
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.Location = new System.Drawing.Point(6, 73);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(73, 17);
+            this.lblCategoria.TabIndex = 3;
+            this.lblCategoria.Text = "Categoria:";
+            // 
+            // lstCatProductos
+            // 
+            this.lstCatProductos.FormattingEnabled = true;
+            this.lstCatProductos.Items.AddRange(new object[] {
+            "Pereceredos",
+            "No pereceredos",
+            "Bebidas",
+            "Gula"});
+            this.lstCatProductos.Location = new System.Drawing.Point(85, 72);
+            this.lstCatProductos.Name = "lstCatProductos";
+            this.lstCatProductos.Size = new System.Drawing.Size(103, 21);
+            this.lstCatProductos.TabIndex = 2;
+            // 
+            // textNombreDeProducto
+            // 
+            this.textNombreDeProducto.Location = new System.Drawing.Point(151, 37);
+            this.textNombreDeProducto.Name = "textNombreDeProducto";
+            this.textNombreDeProducto.Size = new System.Drawing.Size(100, 20);
+            this.textNombreDeProducto.TabIndex = 1;
+            // 
+            // lblNombreDeProducto
+            // 
+            this.lblNombreDeProducto.AutoSize = true;
+            this.lblNombreDeProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreDeProducto.Location = new System.Drawing.Point(6, 37);
+            this.lblNombreDeProducto.Name = "lblNombreDeProducto";
+            this.lblNombreDeProducto.Size = new System.Drawing.Size(146, 17);
+            this.lblNombreDeProducto.TabIndex = 0;
+            this.lblNombreDeProducto.Text = "Nombre del Producto:";
+            this.lblNombreDeProducto.Click += new System.EventHandler(this.label1_Click);
             // 
             // frmAlacenaPaginaDeAgregado
             // 
